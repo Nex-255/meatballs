1aa325a1a6a2b4909a9d76c42c58a677be2f4d19
Nex-255 <johnnyebarrow@gmail.com>
2024-11-06 12:09:29 -0500

    Add pause feature (NOT TESTED)
    Pressing the P key will now pause the sketch, hopefully

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@
 // MetaBalls : p5.js implementation
 
 var blobs = []
+let paused = false
 
 function setup() {
   createCanvas(400, 200);
@@ -13,23 +14,38 @@ function setup() {
 
 function draw() {
   background(51);
-
-  loadPixels();
-  for (x = 0; x < width; x++) {
-    for (y = 0; y < height; y++) {
-      let sum = 0;
-      for (i = 0; i < blobs.length; i++) {
-        let xdif = x - blobs[i].x;
-        let ydif = y - blobs[i].y;
-        let d = sqrt((xdif * xdif) + (ydif * ydif));
-        sum += 10 * blobs[i].r / d;
+  if (paused) {
+    loadPixels();
+    for (x = 0; x < width; x++) {
+      for (y = 0; y < height; y++) {
+        let sum = 0;
+        for (i = 0; i < blobs.length; i++) {
+          let xdif = x - blobs[i].x;
+          let ydif = y - blobs[i].y;
+          let d = sqrt((xdif * xdif) + (ydif * ydif));
+          sum += 10 * blobs[i].r / d;
+        }
+        set(x, y, color(sum, 255, 255));
       }
-      set(x, y, color(sum, 255, 255));
     }
-  }
-  updatePixels();
+    updatePixels();
 
-  for (i = 0; i < blobs.length; i++) {
-    blobs[i].update();
+    for (i = 0; i < blobs.length; i++) {
+      blobs[i].update();
+    }
+  } else {
+    push();
+    fill(15);
+    stroke(80,255,221);
+    textAlign(CENTER,CENTER);
+    text('PAUSED', width/2, height/2);
+    pop();
   }
 }
+
+function keyPressed() {
+  if (keyCode === 80) {
+    paused = !paused;
+  }
+}
+  
