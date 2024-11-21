f2ccab7530a83678e0af1cf25a3147d9406b53cc
Nex-255 <johnnyebarrow@gmail.com>
2024-11-21 11:37:04 -0500

    button's an image now

--- button.js
+++ button.js
@@ -1,20 +1,15 @@
 class Button {
-    constructor(x,y,w,h,text,presstext) {
+    constructor(img,x,y,w,h,presstext) {
+        this.img = img;
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
-        this.text = text;
         this.presstext = presstext;
     }
     draw() {
         push();
-        fill('orange');
-        rect(this.x,this.y,this.w,this.h);
-        fill('black');
-        textAlign(CENTER,CENTER);
-
-        text(this.text, this.x + (this.w/2), this.y + (this.h/2));
+        image(this.img,this.x,this.y,this.w,this.h);
         pop();
     }
     handleClick(mx,my) {
--- sketch.js
+++ sketch.js
@@ -5,15 +5,21 @@
 
 var blobs = []
 let paused = false
-
 const buttons = []
+let image;
+
+let b = new Button(image,50,150,250,100,'willem dafoe smoking cigarette with two oranges one of them moldy');
+buttons.push(b);
+
+function preload() {
+  img = loadImage('/assets/realphoto.jpg')
+}
 
 function setup() {
   createCanvas(400,400);
   colorMode(HSB);
   for (i = 0; i < 10; i++) blobs.push(new Blob(random(0, width), random(0, height)));
-  let b = new Button(50,150,250,100,'what the freak','erm, what the sigma?');
-  buttons.push(b);
+  
 }
 
 function draw() {
