68e39ee756071475b0061968c41e6e587c4dba95
Nex-255 <johnnyebarrow@gmail.com>
2024-12-11 11:40:17 -0500

    less idiotic speed gain

--- blob.js
+++ blob.js
@@ -10,15 +10,15 @@ class Blob {
     this.y = y;
     let angle = random(0, 2 * PI);
     this.xspeed = random(0.4, 1) * Math.cos(angle);
-    this.yspeed = random(2, 5) * Math.sin(angle);
+    this.yspeed = random(0.4, 1) * Math.sin(angle);
     this.r = random(120,180);
   }
 
   update() {
     this.x += this.xspeed;
     this.y += this.yspeed;
-    if (this.x > width || this.x < 0) this.xspeed *= -1.1;
-    if (this.y > height || this.y < 0) this.yspeed *= -1.1;
+    if (this.x > width || this.x < 0) this.xspeed += (-this.xspeed*2) + 0.5;
+    if (this.y > height || this.y < 0) this.yspeed += (-this.yspeed*2) + 0.5;
   }
 
   show() {
