c7da45f024376738305584aee7cec1365a9426ab
Nex-255 <johnnyebarrow@gmail.com>
2024-12-11 11:37:17 -0500

    meatball bounce accel

--- blob.js
+++ blob.js
@@ -9,7 +9,7 @@ class Blob {
     this.x = x;
     this.y = y;
     let angle = random(0, 2 * PI);
-    this.xspeed = random(2, 5) * Math.cos(angle);
+    this.xspeed = random(0.4, 1) * Math.cos(angle);
     this.yspeed = random(2, 5) * Math.sin(angle);
     this.r = random(120,180);
   }
@@ -17,8 +17,8 @@ class Blob {
   update() {
     this.x += this.xspeed;
     this.y += this.yspeed;
-    if (this.x > width || this.x < 0) this.xspeed *= -1;
-    if (this.y > height || this.y < 0) this.yspeed *= -1;
+    if (this.x > width || this.x < 0) this.xspeed *= -1.1;
+    if (this.y > height || this.y < 0) this.yspeed *= -1.1;
   }
 
   show() {
